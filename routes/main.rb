dbcbd98baa0097f2a8d12c6209ac189b551e2fb7
Scott Galetto <scottgaletto@Scotts-iMac.local>
2013-07-11 13:17:28 -0500

    initial commit

--- routes/main.rb
+++ routes/main.rb
@@ -1,9 +1,15 @@
 # encoding: utf-8
 class MyApp < Sinatra::Application
   get "/" do
-    @title = "Welcome to MyApp"
+    @title = "Bobblehead Proto"
     erb :main
   end
+
+  get "/accelo" do
+    @title = "Bobblehead Proto"
+    erb :main2
+  end
+
 
   get "/monitor" do
     erb :monitor, :layout => false
